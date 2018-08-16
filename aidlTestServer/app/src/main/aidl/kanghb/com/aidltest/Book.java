c1665164585d342fe823f0d48edd41e34208dedb
kanghanbin <1975386770@qq.com>
2018-08-16 12:54:58 +0800

    aidlTestServer

---
+++ aidlTestServer/app/src/main/aidl/kanghb/com/aidltest/Book.java
@@ -0,0 +1,79 @@
+package kanghb.com.aidltest;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * 创建时间：2018/8/10
+ * 编写人：kanghb
+ * 功能描述：
+ */
+public class Book implements Parcelable {
+    private int price;
+    private String name;
+
+    public Book() {
+    }
+
+    protected Book(Parcel in) {
+        price = in.readInt();
+        name = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name == null ? "" : name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(price);
+        dest.writeString(name);
+    }
+    /**
+     * 参数是一个Parcel,用它来存储与传输数据
+     * @param dest
+     */
+    public void readFromParcel(Parcel dest) {
+        //注意，此处的读值顺序应当是和writeToParcel()方法中一致的
+        price = dest.readInt();
+        name = dest.readString();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "price=" + price +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
